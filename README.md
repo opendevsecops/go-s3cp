bf30b2fc5379c34c81d5814f8ce46e129382aa50
Petko D. Petkov <pdp@gnucitizen.org>
2019-03-27 11:29:38 +0000

    First.

---
+++ README.md
@@ -0,0 +1,6 @@
+[![Codacy Badge](https://api.codacy.com/project/badge/Grade/b07a461e6a9a48fc84226baefff06423)](https://www.codacy.com/app/OpenDevSecOps/go-s3cp?utm_source=github.com&amp;utm_medium=referral&amp;utm_content=opendevsecops/go-s3cp&amp;utm_campaign=Badge_Grade)
+[![Follow on Twitter](https://img.shields.io/twitter/follow/opendevsecops.svg?logo=twitter)](https://twitter.com/opendevsecops)
+
+# go-s3cp
+
+Simple utility to copy files from/to s3.
---
+++ main.go
@@ -0,0 +1,98 @@
+package main
+
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/awserr"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
+	"github.com/aws/aws-sdk-go/service/s3/s3manager"
+	"log"
+	"net/url"
+	"os"
+	"strings"
+)
+
+func main() {
+	fromPtr := flag.String("from", "", "From source")
+	toPtr := flag.String("to", "", "To destination")
+
+	flag.Parse()
+
+	if strings.HasPrefix(*fromPtr, "s3://") {
+		u, err := url.Parse(*fromPtr)
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		f, err := os.Create(*toPtr)
+
+		if err != nil {
+			log.Fatal(err)
+
+			return
+		}
+
+		downloader := s3manager.NewDownloader(session.New())
+
+		result, err := downloader.Download(f, &s3.GetObjectInput{
+			Bucket: aws.String(u.Host),
+			Key:    aws.String(u.Path),
+		})
+
+		if err != nil {
+			if aerr, ok := err.(awserr.Error); ok {
+				switch aerr.Code() {
+				default:
+					log.Fatal(aerr)
+				}
+			} else {
+				log.Fatal(err)
+			}
+
+			return
+		}
+
+		fmt.Println(result)
+	} else if strings.HasPrefix(*toPtr, "s3://") {
+		u, err := url.Parse(*toPtr)
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		f, err := os.Open(*fromPtr)
+
+		if err != nil {
+			log.Fatal(err)
+
+			return
+		}
+
+		uploader := s3manager.NewUploader(session.New())
+
+		_, err = uploader.Upload(&s3manager.UploadInput{
+			Bucket: aws.String(u.Host),
+			Key:    aws.String(u.Path),
+			Body:   aws.ReadSeekCloser(bufio.NewReader(f)),
+		})
+
+		if err != nil {
+			if aerr, ok := err.(awserr.Error); ok {
+				switch aerr.Code() {
+				default:
+					log.Fatal(aerr)
+				}
+			} else {
+				log.Fatal(err)
+			}
+
+			return
+		}
+	} else {
+		log.Fatal("unsupported operation")
+	}
+}
